cada372ecb19b27edec7adffe79e76de2c167a58
wkmikw <wkmikw@hotmail.com>
2017-09-16 23:38:42 +0800

    weather

--- die_visual.py
+++ die_visual.py
@@ -1,17 +1,35 @@
 #coding=utf-8
 #python die_visual.py
 from die import Die 
+import pygal
 
-die = Die()
+die_1 = Die()
+die_2 = Die(10)
 #save in a list
 results = []
-for roll_num in range(1000):
-	result = die.roll()
+for roll_num in range(10000):
+	result = die_1.roll() + die_2.roll()
 	results.append(result)
 
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
 	frequency = results.count(value)
 	frequencies.append(frequency)
 
-print(frequencies)
+#visual
+hist = pygal.Bar()
+
+hist.title = "Results of rolling  D6+D10 1000 times"
+outnum = []
+for i in range(2,max_result+1):
+	outnum.append(str(i))
+hist.x_labels = outnum
+hist.x_title = "Results"
+hist.y_title = "Frequency of Results"
+
+hist.add('D6 + D10', frequencies)
+hist.render_to_file('die_visual.svg')
+
+#print(frequencies)
+
---
+++ highs_lows.py
@@ -0,0 +1,38 @@
+#coding=utf-8
+#python highs_lows.py
+
+import csv
+from matplotlib import pyplot as plt 
+from datetime import datetime
+
+def FtoC(degree):
+	return(round(((int(degree)-32)/1.8),1))
+
+filename = 'weather.csv'
+with open(filename) as f:
+	reader = csv.reader(f)
+	header_row = next(reader)
+	
+	dates, highs ,lows= [], [], []
+	for row in reader:
+		current_date = datetime.strptime(row[0], "%Y-%m-%d")
+		dates.append(current_date)
+		highs.append(FtoC(row[1]))
+		lows.append(FtoC(row[2]))
+
+	#print(highs)
+
+#plot data
+fig = plt.figure(dpi=141, figsize=(10,6))
+plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
+#settings of fig
+title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
+plt.title(title, fontsize=20)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel("Temperature (C)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+
+plt.show()
